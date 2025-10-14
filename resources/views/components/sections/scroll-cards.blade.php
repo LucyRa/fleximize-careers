21e6da5e328b28bc0d1afe21088bee2708a68c71
Lucy Ramplin <lucyramplin@gmail.com>
2025-10-14 14:26:47 +0100

    Ensuring that scroll-cards are keyboard accessible

--- resources/views/components/sections/scroll-cards.blade.php
+++ resources/views/components/sections/scroll-cards.blade.php
@@ -1,12 +1,12 @@
 <section class="scroll-cards">
     <div class="container heading">
-        <h2>The Perks</h2>
+        <h2 id="scroll-cards-title">The Perks</h2>
         <p>(Scroll to see more) <span><x-icons.arrow-full fill="transparent" color="#daf1fa" /></span></p>
     </div>
 
-    <ul class="track">
+    <ul class="track" role="tablist" aria-labelledby="scroll-cards-title">
         @foreach($cards as $card)
-        <li class="scroll-card">
+        <li class="scroll-card" role="tabpanel" tabindex="0">
             <x-dynamic-component :component="'icons.' . $card['icon']" />
 
             <h3>{{ $card['title'] }}</h3>
