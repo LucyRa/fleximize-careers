16ccfb33af1892949a077333d269f40aa05644cf
Lucy Ramplin <lucyramplin@gmail.com>
2025-10-15 00:37:52 +0100

    Adding js to breadcrumbs

--- resources/views/livewire/breadcrumbs.blade.php
+++ resources/views/livewire/breadcrumbs.blade.php
@@ -1,4 +1,15 @@
-<div class="breadcrumbs">
+<div class="breadcrumbs" x-data="{
+    visible: true,
+    toggleOnScroll() {
+        if (window.scrollY >= 200 && this.visible) {
+            this.visible = false;
+        }
+
+        if (window.scrollY <= 500 && !this.visible) {
+            this.visible = true;
+        }
+    }
+}" @scroll.window="toggleOnScroll" x-show="visible" x-transition.duration.500ms x-transition.opacity>
     <ul>
         <li><a href="" class="icon-round"><x-icons.house-chimney-full color="#DAF1FA" /></a></li>
         <li>About us</li>
