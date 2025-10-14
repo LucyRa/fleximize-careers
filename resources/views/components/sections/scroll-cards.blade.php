0dfa3d7cf1ed86442f35d595a01abd0f854c75ac
Lucy Ramplin <lucyramplin@gmail.com>
2025-10-14 23:36:30 +0100

    Adjusting responsive styling on scroll cards

--- resources/views/components/sections/scroll-cards.blade.php
+++ resources/views/components/sections/scroll-cards.blade.php
@@ -4,15 +4,17 @@
         <p>(Scroll to see more) <span><x-icons.arrow-full fill="transparent" color="#daf1fa" /></span></p>
     </div>
 
-    <ul class="track" role="tablist" aria-labelledby="scroll-cards-title">
-        @foreach($cards as $card)
-        <li class="scroll-card" role="tabpanel" tabindex="0">
-            <x-dynamic-component :component="'icons.' . $card['icon']" />
+    <div class="scroll-cards__wrapper">
+        <ul class="track" role="tablist" aria-labelledby="scroll-cards-title">
+            @foreach($cards as $card)
+            <li class="scroll-card" role="tabpanel" tabindex="0">
+                <x-dynamic-component :component="'icons.' . $card['icon']" />
 
-            <h3>{{ $card['title'] }}</h3>
+                <h3>{{ $card['title'] }}</h3>
 
-            <p>{{ $card['blurb'] }}</p>
-        </li>
-        @endforeach
-    </ul>
+                <p>{{ $card['blurb'] }}</p>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </section>
